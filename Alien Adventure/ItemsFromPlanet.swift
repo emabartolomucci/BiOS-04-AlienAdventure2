aa5fab65246160abf523a4c14ae4b3cebd617293
Emanuele Bartolomucci <e@emanuelebartolomucci.com>
2016-08-15 13:33:29 +0100

    Initial commit

---
+++ Alien Adventure/ItemsFromPlanet.swift
@@ -0,0 +1,19 @@
+//
+//  ItemsFromPlanet.swift
+//  Alien Adventure
+//
+//  Edited by Emanuele Bartolomucci on 2016/08/15.
+//
+
+extension Hero {
+    
+    func itemsFromPlanet(inventory: [UDItem], planet: String) -> [UDItem] {
+        
+        
+        
+        return [UDItem]()
+    }
+    
+}
+
+// If you have completed this function and it is working correctly, feel free to skip this part of the adventure by opening the "Under the Hood" folder, and making the following change in Settings.swift: "static var RequestsToSkip = 1"
