f30ad278d3391f54274d6577d6c31500c624a647
Emanuele Bartolomucci <e@emanuelebartolomucci.com>
2016-08-16 08:41:52 +0100

    ItemComparison

--- Alien Adventure/ItemComparison.swift
+++ Alien Adventure/ItemComparison.swift
@@ -2,14 +2,25 @@
 //  ItemComparison.swift
 //  Alien Adventure
 //
-//  Created by Jarrod Parkes on 10/4/15.
-//  Copyright © 2015 Udacity. All rights reserved.
+//  Edited by Emanuele Bartolomucci on 2016/08/16.
 //
 
 import Foundation
 
 func <(lhs: UDItem, rhs: UDItem) -> Bool {
-    return true
+    
+    if lhs.rarity.rawValue < rhs.rarity.rawValue {
+        
+        return true
+        
+    } else if (lhs.rarity.rawValue == rhs.rarity.rawValue) {
+        
+        if (lhs.baseValue < rhs.baseValue) {
+            
+            return true
+            
+        }
+    }
+    
+    return false
 }
-
-// If you have completed this function and it is working correctly, feel free to skip this part of the adventure by opening the "Under the Hood" folder, and making the following change in Settings.swift: "static var RequestsToSkip = 5"
