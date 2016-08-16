735720238ef6e26ee446d04489486c482fda8776
Emanuele Bartolomucci <e@emanuelebartolomucci.com>
2016-08-16 10:19:09 +0100

    MostCommonCharacter

--- Alien Adventure/MostCommonCharacter.swift
+++ Alien Adventure/MostCommonCharacter.swift
@@ -2,13 +2,42 @@
 //  MostCommonCharacter.swift
 //  Alien Adventure
 //
-//  Created by Jarrod Parkes on 10/4/15.
-//  Copyright © 2015 Udacity. All rights reserved.
+//  Edited by Emanuele Bartolomucci on 2016/08/16.
 //
 
 extension Hero {
     
     func mostCommonCharacter(inventory: [UDItem]) -> Character? {
-        return nil
+        
+        var mostCommonChar = Character?()
+        var charCount = Int()
+        var charDict = [Character:Int]()
+        
+        for item in inventory {
+            
+            for char in item.name.lowercaseString.characters {
+                
+                if let _ = charDict[char] {
+                    
+                    charDict[char] = charDict[char]! + 1
+                    
+                } else {
+                    
+                    charDict[char] = 0
+                    
+                }
+            }
+        }
+        
+        for (charName, count) in charDict {
+            
+            if count > charCount {
+                charCount = count
+                mostCommonChar = charName
+            }
+            
+        }
+        
+        return mostCommonChar
     }
 }
